d39845f85a96eb2e6c0b71f1c091053485caaa89
guidofuens <guidofuens@gmail.com>
2023-11-27 09:07:07 -0300

    clase 20-11

--- a_prueba/urls.py
+++ a_prueba/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index)
+    path('', index),
+    path('contacto/', contacto, name='contacto')
 
 
 ]
--- a_prueba/views.py
+++ a_prueba/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     template_name = 'index.html'
-    return render(request, template_name)
+    context = {'valor':50}
+    return render(request, template_name, context)
     
+def contacto(request):
+    template_name = 'contacto.html'
+    nombres = ['Informatorio', 'Guido', 'Landers']
+    context = {'nombres':nombres}
+    return render(request, template_name, context)
